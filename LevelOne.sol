db03cb3dffbc2f6c8741c5b4d769bc8bc44ac985
Nikita <v.nikit@gmail.com>
2017-11-03 18:03:36 +0300

    Заплатить и показать

--- LevelOne.sol
+++ LevelOne.sol
@@ -1,7 +1,10 @@
+pragma solidity ^0.4.15;
+
 contract LessonFirst {
     uint256 public number;
 
     function () payable {
-        number = block.number;
+        if (msg.value >= 1 * 0.1 ether)
+            number = block.number;
     }
 }
